99147b64f544d0de66011472086dc048362d5671
Fiils <ioanalexandruipatov12@gmail.com>
2022-04-29 17:58:41 +0300

    fixed error

--- config/server.ts
+++ config/server.ts
@@ -1,4 +1,4 @@
 export const dev: boolean = process.env.NODE_ENV !== 'production';
 
-export const server: string = dev ? 'http://localhost:9999' : 'https://romdigcoserver.com'
-export const client: string = dev ? 'http://localhost:3000' : 'https://romdig.net'
+export const server: string = dev ? 'http://localhost:9999' : 'https://www.romdigcoserver.com'
+export const client: string = dev ? 'http://localhost:3000' : 'https://www.romdig.net'

56537a93707ff50376e38ca6ac6735376ea964a6
Fiils <ioanalexandruipatov12@gmail.com>
2023-05-20 10:34:36 +0300

    fixed error

--- config/server.ts
+++ config/server.ts
@@ -1,4 +1,4 @@
 export const dev: boolean = process.env.NODE_ENV !== 'production';
 
-export const server: string = dev ? 'http://localhost:9999' : 'web-production-9d8f.up.railway.app'
+export const server: string = dev ? 'http://localhost:9999' : 'https://web-production-9d8f.up.railway.app'
 export const client: string = dev ? 'http://localhost:3000' : 'https://www.romdig.net'
